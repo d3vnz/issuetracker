e1e7877700b53225e154fc44614d71adb4aa37a3
aotearoait <33107115+aotearoait@users.noreply.github.com>
2024-09-26 10:47:19 +1200

    Work on vital changes to get working

--- database/migrations/2024_09_16_101238_create_issue_notes_table.php
+++ database/migrations/2024_09_16_101238_create_issue_notes_table.php
@@ -6,7 +6,7 @@
  *
  */
 
-use App\Models\Issue;
+use D3vnz\IssueTracker\Models\Issue;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
